a4ea4848aeac2f3beb12459f3aba5206cc4fb72c
cburbs <60953037+cburbs@users.noreply.github.com>
2023-12-05 09:47:17 -0800

    Create O365_Download.ps1

---
+++ O365/O365_Download.ps1
@@ -0,0 +1,73 @@
+Try
+{
+#===================================================================================================
+# OfficeMove
+#===================================================================================================
+$source = "$Yoursharename\Office365\Office"
+$destold = "$Yoursharename\Office365\Old"
+$dest = "$Yoursharename\Office365\Old"
+write-infolog "***** Deleting files from OLD folder *****" -Verbose
+remove-Item -path $destold -force -exclude old -recurse -confirm:$false
+Start-Sleep -Seconds 20
+write-infolog "*****Moving Office files to OLD folder*****" -Verbose
+Get-ChildItem -Path $SOURCE | Move-Item -Destination $DEST
+Get-ChildItem -Path $SOURCE -Recurse -Directory | Remove-Item -confirm $false 
+Start-Sleep -Seconds 20
+
+#===================================================================================================
+# OfficeDownload
+#===================================================================================================
+
+$DownloadsPath = "$Yoursharename\Office365"
+$OfficeODT = '$Yoursharename\Office365\ODT\setup.exe'
+$downloadsodt = '$Yoursharename\Office365\ODT'
+$ODTOfficeProduct = 'O365ProPlusRetail'
+$OfficeArch = '64'
+$LanguageId ="en-us"
+
+$ODTXml = @"
+<Configuration>
+<Add SourcePath="$DownloadsPath" OfficeClientEdition="$OfficeArch" Channel="MonthlyEnterprise">
+    <Product ID="$ODTOfficeProduct">
+        <Language ID="$LanguageId" />
+    </Product>
+    </Add>
+</Configuration>
+"@
+
+    if (!(Test-Path $downloadsodt)) {New-Item $downloadsodt -ItemType Directory -Force | Out-Null}
+    $ODTXml | Out-File "$downloadsodt\OSDUpdate.xml" -Encoding utf8 -Force
+    
+    if (!($XmlOnly)) {
+        write-infolog "*****Downloading ... This may take a while *****" -Verbose
+        
+        Start-Process -FilePath "$OfficeODT" -ArgumentList "/download","`"$downloadsodt\OSDUpdate.xml`"" -Wait -NoNewWindow
+       
+    }
+    write-infolog "***** Download Completed! *****" -Verbose
+    }
+Catch {
+        Write-ErrorLog "***** Error Downloading Office File! *****" $_.Exception.message
+        $errorbody = "$_.Exception.message"
+        [string]$subject = "Officedownload Catch Error!"
+        Send-Email $errorbody $subject
+       } #catch end
+############################
+# Email IT Term Complete   #
+############################
+$datasource = "$Yoursharename\Office365\Office\Data"
+$version = (Get-ChildItem -Path $dataSOURCE -Recurse -Directory).name
+	tRY {
+	    Write-infolog ""
+		  Write-infolog "***** Email IT Download Complete ******" 
+      $subject = "Latest Office Download has completed!"
+		[string]$body = "The latest Monthly Enterprise version ($version) of Office has downloaded. "
+		send-mailmessage -from $emailaddress -To $emailaddress  -SmtpServer $mailserver -Body $body -Subject $subject }
+	Catch {
+		  Write-ErrorLog "(Error!) sending email Function Email-Error!!" $_.Exception.message
+		  $errorbody = "$_.Exception.message"
+      [string]$subject = "Email Catch Error!"
+      Send-Email $errorbody $subject		  
+		  }
+
+Close-logger
