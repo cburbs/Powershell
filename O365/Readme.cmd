7161459330a163759e3f0912e8625be215d52d68
cburbs <60953037+cburbs@users.noreply.github.com>
2023-12-05 09:55:32 -0800

    Update Readme.cmd

--- O365/Readme.cmd
+++ O365/Readme.cmd
@@ -3,11 +3,15 @@ I have just started playing with Poshlog for better logging - https://github.com
 Patch_Tuesday.ps1 script is run monthly on my Sched server in Task Scheduler. I have it run starting on day 6 through 17 as patch Tuesday for most months seems to fall in that range.
 If it is the Wed after Patch Tuesday then the script calls on the O365_Download.ps1 script
 
+########################################################
 Patch_Tuesday.ps1 script variables you will need to set:
+########################################################
 - $logpath
 - The line to call the 0365 script which is right after "Calling O365 Download"
 
+########################################################
 O365_Download.ps1 script variables you will need to set:
+########################################################
 #===================================================================================================
 # OfficeMove
 #===================================================================================================
