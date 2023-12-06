1d2a0daf64d472480802931b6ca04fd65b531aa9
cburbs <60953037+cburbs@users.noreply.github.com>
2023-12-06 07:29:30 -0800

    Update Connections.cmd

--- O365/Connections.cmd
+++ O365/Connections.cmd
@@ -1,1 +1,18 @@
-
+|Product|	Connection Command|	Disconnect Command	|Show Connected	Work| with MFA
+Teams	Connect-MicrosoftTeams	Disconnect-MicrosoftTeams		
+				
+Azure AD				
+				
+Office 365				
+				
+Intune				
+				
+Security				
+				
+Compliance				
+				
+Identity				
+				
+Exchange				
+				
+Sharepoint	Connect-SPOService	Disconnect-SPOService	get-sposite	
