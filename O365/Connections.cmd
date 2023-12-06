cb67cc28db5705acc795e989bada15e6b6f1f3b3
cburbs <60953037+cburbs@users.noreply.github.com>
2023-12-06 07:29:51 -0800

    Update Connections.cmd

--- O365/Connections.cmd
+++ O365/Connections.cmd
@@ -1,18 +1,4 @@
-|Product|	Connection Command|	Disconnect Command	|Show Connected	Work| with MFA
-Teams	Connect-MicrosoftTeams	Disconnect-MicrosoftTeams		
-				
-Azure AD				
-				
-Office 365				
-				
-Intune				
-				
-Security				
-				
-Compliance				
-				
-Identity				
-				
-Exchange				
-				
-Sharepoint	Connect-SPOService	Disconnect-SPOService	get-sposite	
+| Command | Description |
+| --- | --- |
+| git status | List all new or modified files |
+| git diff | Show file differences that haven't been staged |
